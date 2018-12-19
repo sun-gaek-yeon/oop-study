2574eb213e1bc021ca39e211e12929ffa7f081d2
이원섭wonsup Lee <onesup.lee@gmail.com>
2018-12-19 20:52:59 +0900

    chapter 2 실습

---
+++ chapter-2/wonsup-ratio-3.rb
@@ -0,0 +1,25 @@
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+    @rim = rim
+    @tire = tire
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    ratio * ( rim + ( tire * 2 ) )
+  end
+
+  def t
+    "#{3}" + "3"
+  end
+end
+
+puts Gear.new(52, 11, 26, 1.5).gear_inches
+puts Gear.new(52, 11, 24, 1.25).gear_inches
+puts Gear.new(52, 11).ratio
---
+++ chapter-2/wonsup-ratio-4.rb
@@ -0,0 +1,13 @@
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+end
+
+puts Gear.new(52, 11).ratio
